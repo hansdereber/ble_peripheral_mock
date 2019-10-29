ad43caea506b9272c35b386b6589253318c16cb0
Christian Geisler <christian.geisler@posteo.de>
2019-10-29 15:39:39 +0100

    Set notification

--- examples/fitness-machine-mock/service/indoor-bike-data-characteristic.js
+++ examples/fitness-machine-mock/service/indoor-bike-data-characteristic.js
@@ -5,8 +5,6 @@ var bleno = require('../../..')
 var Descriptor = bleno.Descriptor
 var Characteristic = bleno.Characteristic
 
-var InstantaneousSpeedCharacteristic = require('../indoor-bike-data/instantaneous-speed-characteristic');
-
 var IndoorBikeDataCharacteristic = function () {
     IndoorBikeDataCharacteristic.super_.call(this, {
         uuid: '13333333333333333333333333330002',
@@ -17,16 +15,27 @@ var IndoorBikeDataCharacteristic = function () {
                 value: 'This holds the training-related data of an indoor bike',
             })
         ],
-        characteristics: [
-            new InstantaneousSpeedCharacteristic(),
-        ]
     })
+
+    this
 }
 
 util.inherits(IndoorBikeDataCharacteristic, Characteristic)
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
 IndoorBikeDataCharacteristic.prototype.onReadRequest = function (offset, callback) {
-    callback(this.RESULT_SUCCESS, new Buffer('finger'))
+    setTimeout(function() {
+        if (self.updateValueCallback) {
+            var data = new Buffer(1);
+            data.writeUInt8(getRandomInt(2), 0);
+            self.updateValueCallback(data);
+        }
+    }, 1000);
+
+    callback(this.RESULT_SUCCESS)
 }
 
 module.exports = IndoorBikeDataCharacteristic
